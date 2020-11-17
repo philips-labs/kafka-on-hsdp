df37e1d87604310eb659ab45571bdd3f453212cc
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2020-11-17 18:47:54 +0100

    Switch to philips-labs/cloudfoundry

--- terraform.tf
+++ terraform.tf
@@ -1,15 +1,15 @@
 terraform {
   required_providers {
     cloudfoundry = {
-      source  = "cloudfoundry-community/cloudfoundry"
-      version = ">= 0.12.4"
+      source  = "philips-labs/cloudfoundry"
+      version = ">= 0.1206.0"
     }
     random = {
       source = "random"
     }
     hsdp = {
       source  = "philips-software/hsdp"
-      version = ">= 0.6.6"
+      version = ">= 0.7.0"
     }
   }
 }
